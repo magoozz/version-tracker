2782b1beacab739c6a26c1f765dc4520e9b93048
Megan Bartlett <77411660+magoozz@users.noreply.github.com>
2025-03-15 16:57:25 -0500

    Create TrackerData.java

---
+++ TrackerData.java
@@ -0,0 +1,12 @@
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TrackerData {
+
+    private String productName;
+    private String environment;
+    private String componentName;
+    private String componentVersion;
+
+}
